370cb92a9104121600e13e31e5f9303d82b255db
Monique Hayes <moniqueehayes5@gmail.com>
2023-03-27 14:30:09 -0500

    styling fixed

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import Home from "./Home";
 import MyQuotes from "./MyQuotes";
+import './style.css'; 
+import logo from './logo.png';
 
 const Main = () => {
     return (
         <div>
             <nav>
-                <img src='logo.png'/>
+                <img src={logo}/>
                 <div id='links'>
                 <Link id='home' to='/'>Home</Link>
                 <Link id='myquotes' to='/myquotes'>My Quotes</Link>
